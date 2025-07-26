cf59dcd9cfb8c4bb07702958d34cc69cc7071520
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-26 16:42:22 +0530

    Delete ecr directory

--- ecr/output.tf
+++
@@ -1,6 +0,0 @@
-output "repositories" {
-    value = {
-        for repo, resource in aws_ecr_repository.ecr : 
-        repo => resource.repository_url
-    }
-}
--- ecr/variable.tf
+++
@@ -1,4 +0,0 @@
-variable "repo_name" {
-    type = list(string)
-    description = "List of ECR repo names"
-}

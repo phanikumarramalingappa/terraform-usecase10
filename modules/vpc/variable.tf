eabb027217833e81125565cba66254071cff63ba
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-26 16:46:30 +0530

    Create variable.tf

---
+++ modules/vpc/variable.tf
@@ -0,0 +1,25 @@
+variable "vpc_name" {
+  description = "VPC name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}

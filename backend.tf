cae154b682c9f0b3902ff4a7a75842e5e21e4425
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-26 16:51:43 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket       = "terraform-usecases-nsh-batch-6"
+    key          = "usecase10/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+}

9905003cc21b70e2362bfae2a34e73df95a210c2
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-27 23:47:56 +0530

    Update main.tf

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -1,12 +1,13 @@
 resource "aws_security_group" "alb_security_group" {
   name        = var.alb_sg_name
-  description = "Allow HTTP traffic only from ALB"
+  description = "Security group for internal ALB"
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "Allow HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -18,20 +19,28 @@ resource "aws_security_group" "alb_security_group" {
   }
 }
 
-resource "aws_security_group" "security_group" {
-  name        = "uc10-sg"
-  description = "Allow HTTP traffic only from ALB"
+resource "aws_security_group" "ecs_security_group" {
+  name        = "ecs-service-sg"
+  description = "Allow traffic from ALB to ECS containers"
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    security_groups = [aws_security_group.alb_security_group.id]
+    on port 3000"
+    from_port                = 3000
+    to_port                  = 3000
+    protocol                 = "tcp"
+    security_groups          = [aws_security_group.alb_security_group.id]
   }
 
-  egress {
-    from_port   = 0
+  ingress {
+    description              = "Allow traffic from ALB on port 3001"
+    from_port                = 3001
+    to_port                  = 3001
+    protocol                 = "tcp"
+    security_groups          = [aws_security_group.alb_security_group.id]
+  }
+
+ from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]

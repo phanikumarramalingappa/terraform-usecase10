f7a83c236a7e484ba9e290b0403b181089e72a2a
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-27 13:02:18 +0530

    Update output.tf

--- modules/apigateway/output.tf
+++ modules/apigateway/output.tf
@@ -2,8 +2,10 @@ output "rest_api_id" {
   value = aws_api_gateway_rest_api.main.id
 }
 
+data "aws_region" "current" {}
+
 output "api_gateway_url" {
-  value = "https://${aws_api_gateway_rest_api.main.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}"
+  value = "https://${aws_api_gateway_rest_api.main.id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}"
 }
 
 output "authorizer_id" {

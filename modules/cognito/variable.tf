43945361a474223e19c8a53a5a8f4cfdfbf3b6ea
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-27 10:17:48 +0530

    Update variable.tf

--- modules/cognito/variable.tf
+++ modules/cognito/variable.tf
@@ -1,1 +1,15 @@
+variable "user_pool_name" {
+  description = "Name of the Cognito User Pool"
+  type        = string
+}
+
+variable "user_pool_client_name" {
+  description = "Name of the Cognito User Pool Client"
+  type        = string
+}
+
+variable "cognito_domain_prefix" {
+  description = "Prefix for Cognito domain"
+  type        = string
+}
 

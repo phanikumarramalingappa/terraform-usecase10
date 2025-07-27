bbef595e77d9d2ff7aeecc45b7d5eb78c147cf31
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-27 23:29:12 +0530

    Update output.tf

--- modules/alb/output.tf
+++ modules/alb/output.tf
@@ -2,6 +2,9 @@ output "alb_dns_name" {
   value = aws_lb.this.dns_name
 }
 
+output "alb_arn" {
+  value = aws_lb.this.arn
+}
 
 output "appointments_tg_arn" {
   value = aws_lb_target_group.appointments.arn

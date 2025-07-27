f14648dae5f24d221b1e902bcd8e8870d3205d3b
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-28 00:05:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "alb" {
   source         = "./modules/alb"
   name           = "mc-alb"
   vpc_id         = module.vpc.vpc_id
-  public_subnets = module.vpc.public_subnet_ids
+  private_subnets = module.vpc.private_subnet_ids
   alb_sg_id      = module.sg.alb_sg_id
 }
 

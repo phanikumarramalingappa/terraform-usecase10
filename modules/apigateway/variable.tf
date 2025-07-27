f227d05bd5ef4d9bd26f90bfdeea55716fdd9f08
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-27 10:19:35 +0530

    Update variable.tf

--- modules/apigateway/variable.tf
+++ modules/apigateway/variable.tf
@@ -1,1 +1,24 @@
+variable "api_name" {
+  description = "Name of the API Gateway"
+  type        = string
+}
 
+variable "alb_listener_arn" {
+  description = "ARN of the ALB listener"
+  type        = string
+}
+
+variable "alb_dns_name" {
+  description = "DNS name of the ALB"
+  type        = string
+}
+
+variable "user_pool_id" {
+  description = "ID of the Cognito User Pool"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}

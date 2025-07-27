4b3577fae4cd3c9bf2b9080f609de27fb5ef217e
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-27 12:58:28 +0530

    Update output.tf

--- modules/apigateway/output.tf
+++ modules/apigateway/output.tf
@@ -3,7 +3,7 @@ output "rest_api_id" {
 }
 
 output "api_gateway_url" {
-  value = aws_api_gateway_deployment.main.invoke_url
+  value = "https://${aws_api_gateway_rest_api.main.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}"
 }
 
 output "authorizer_id" {

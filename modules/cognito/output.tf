f991766267c6c80b0526bc64306129cd25b203e0
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-27 10:23:24 +0530

    Update output.tf

--- modules/cognito/output.tf
+++ modules/cognito/output.tf
@@ -1,12 +1,15 @@
 output "user_pool_id" {
-  value = aws_cognito_user_pool.this.id
+  value = aws_cognito_user_pool.main.id
+}
+
+output "user_pool_arn" {
+  value = aws_cognito_user_pool.main.arn
 }
 
 output "user_pool_client_id" {
-  value = aws_cognito_user_pool_client.this.id
+  value = aws_cognito_user_pool_client.main.id
 }
 
 output "cognito_domain" {
-  value = aws_cognito_user_pool_domain.this.domain
+  value = aws_cognito_user_pool_domain.main.domain
 }
-

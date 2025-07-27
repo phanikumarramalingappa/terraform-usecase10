339fab4777926392f57497dd4da1bf71e76c7653
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-27 10:21:48 +0530

    Update output.tf

--- modules/apigateway/output.tf
+++ modules/apigateway/output.tf
@@ -1,1 +1,11 @@
+output "rest_api_id" {
+  value = aws_api_gateway_rest_api.main.id
+}
 
+output "api_gateway_url" {
+  value = aws_api_gateway_deployment.main.invoke_url
+}
+
+output "authorizer_id" {
+  value = aws_api_gateway_authorizer.cognito.id
+}

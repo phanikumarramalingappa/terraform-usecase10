a72572ea2bdffd407118610a35f80e1661b9e2bd
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-28 10:38:38 +0530

    Create output.tf

---
+++ modules/nlb/output.tf
@@ -0,0 +1,15 @@
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "alb_arn" {
+  value = aws_lb.this.arn
+}
+
+output "appointments_tg_arn" {
+  value = aws_lb_target_group.appointments.arn
+}
+
+output "patients_tg_arn" {
+  value = aws_lb_target_group.patients.arn
+}

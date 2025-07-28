eb6d6d8af4768f4a2ddaa61d96d2dd5f264a88fb
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-28 10:38:15 +0530

    Create main.tf

---
+++ modules/nlb/main.tf
@@ -0,0 +1,70 @@
+resource "aws_lb" "this" {
+  name               = var.name
+  internal           = true
+  load_balancer_type = "network"
+  subnets            = var.private_subnets
+
+  tags = {
+    Name = var.name
+  }
+}
+
+# Target Groups
+resource "aws_lb_target_group" "patients" {
+  name         = "${var.name}-tg-patients"
+  port         = 3000
+  protocol     = "TCP"
+  vpc_id       = var.vpc_id
+  target_type  = "ip"
+
+  health_check {
+    protocol            = "TCP"
+    port                = "traffic-port"
+    interval            = 30
+    timeout             = 10
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    # matcher removed because it's invalid for TCP
+  }
+}
+
+resource "aws_lb_target_group" "appointments" {
+  name         = "${var.name}-tg-appointments"
+  port         = 3001
+  protocol     = "TCP"
+  vpc_id       = var.vpc_id
+  target_type  = "ip"
+
+  health_check {
+    protocol            = "TCP"
+    port                = "traffic-port"
+    interval            = 30
+    timeout             = 10
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    # matcher removed because it's invalid for TCP
+  }
+}
+
+# Listeners
+resource "aws_lb_listener" "patients" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 3000
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.patients.arn
+  }
+}
+
+resource "aws_lb_listener" "appointments" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 3001
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.appointments.arn
+  }
+}

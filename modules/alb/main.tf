e64892213d5ebda14926a2ca22b94e1c9ed0ed17
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-28 10:40:16 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,7 +1,8 @@
 resource "aws_lb" "this" {
   name               = var.name
   internal           = true
-  load_balancer_type = "network"
+  load_balancer_type = "application"
+  security_groups    = [var.alb_sg_id]
   subnets            = var.private_subnets
 
   tags = {
@@ -10,61 +11,88 @@ resource "aws_lb" "this" {
 }
 
 # Target Groups
+
 resource "aws_lb_target_group" "patients" {
-  name         = "${var.name}-tg-patients"
-  port         = 3000
-  protocol     = "TCP"
-  vpc_id       = var.vpc_id
-  target_type  = "ip"
+  name     = "${var.name}-tg-patients"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "ip"
 
   health_check {
-    protocol            = "TCP"
-    port                = "traffic-port"
-    interval            = 30
-    timeout             = 10
-    healthy_threshold   = 2
+    path = "/health"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 30
+    timeout = 5
+    healthy_threshold = 2
     unhealthy_threshold = 2
-    # matcher removed because it's invalid for TCP
   }
 }
 
 resource "aws_lb_target_group" "appointments" {
-  name         = "${var.name}-tg-appointments"
-  port         = 3001
-  protocol     = "TCP"
-  vpc_id       = var.vpc_id
-  target_type  = "ip"
+  name     = "${var.name}-tg-appointments"
+  port     = 3001
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "ip"
 
-  health_check {
-    protocol            = "TCP"
-    port                = "traffic-port"
-    interval            = 30
-    timeout             = 10
-    healthy_threshold   = 2
+    health_check {
+    path = "/health"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 30
+    timeout = 5
+    healthy_threshold = 2
     unhealthy_threshold = 2
-    # matcher removed because it's invalid for TCP
   }
 }
 
-# Listeners
-resource "aws_lb_listener" "patients" {
+# Listener
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
-  port              = 3000
-  protocol          = "TCP"
+  port              = 80
+  protocol         = "HTTP"
 
   default_action {
-    type             = "forward"
+    type             = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Please enter valid path"
+      status_code = "503"
+    }
+  }
+}
+
+
+resource "aws_lb_listener_rule" "patients" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 10
+
+  action {
+    type = "forward"
     target_group_arn = aws_lb_target_group.patients.arn
   }
+
+  condition {
+    path_pattern {
+      values = ["/patients"]
+    }
+  }
 }
 
-resource "aws_lb_listener" "appointments" {
-  load_balancer_arn = aws_lb.this.arn
-  port              = 3001
-  protocol          = "TCP"
+resource "aws_lb_listener_rule" "appointments" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 11
 
-  default_action {
-    type             = "forward"
+  action {
+    type = "forward"
     target_group_arn = aws_lb_target_group.appointments.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/appointments"]
+    }
   }
 }
